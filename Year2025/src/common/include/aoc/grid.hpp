280a8cde96ea6e16a9ad7b9a95cbd778da46e94a
Ray Tang <30689714+raytangRT@users.noreply.github.com>
2025-12-05 23:11:48 +0800

    Day 4 completed

--- Year2025/src/common/include/aoc/grid.hpp
+++ Year2025/src/common/include/aoc/grid.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <cstddef>
+#include <functional>
+#include <vector>
+
+namespace aoc {
+
+using RowIdx = std::size_t;
+using ColIdx = std::size_t;
+
+class Grid {
+private:
+  std::vector<std::vector<char>> m_grid;
+
+  size_t m_width;
+  size_t m_height;
+
+public:
+  Grid(const std::string &filePath);
+
+  void printGrid() const;
+
+  template <typename Func> void forEach(Func &&fn) const {
+    for (RowIdx i = 0; i < m_height; ++i) {
+      for (ColIdx j = 0; j < m_width; ++j) {
+        fn(i, j, m_grid[i][j]);
+      }
+    }
+  }
+
+  void set(const RowIdx, const ColIdx, const char &);
+
+  std::vector<char> neighbors(RowIdx, ColIdx) const;
+};
+} // namespace aoc
--- Year2025/src/day04/test.cpp
+++ Year2025/src/day04/test.cpp
@@ -1,5 +1,6 @@
 #include "aoc/gtestHelpers.hpp"
 #include "solution.hpp"
+#include <gtest/gtest.h>
 
 using namespace Day04;
 
@@ -10,9 +11,18 @@ TEST(Day04, Part1DEMO) {
 
 TEST(Day04, Part1Actual) {
   long output = solve(false);
-  EXPECT_EQ(output, 13);
+  EXPECT_EQ(output, 1370);
 }
 
+TEST(Day04, Part2DEMO) {
+  long output = solve(true, true);
+  EXPECT_EQ(output, 43);
+}
+
+TEST(Day04, Part2Actual) {
+  long output = solve(false, true);
+  EXPECT_EQ(output, 8437);
+}
 int main(int argc, char *argv[]) {
-  return aoc::runUnitTests(argc, argv, "Day04");
+  return aoc::runUnitTests(argc, argv, "Day04", true);
 }
